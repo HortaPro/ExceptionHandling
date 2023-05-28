1be92a2bccb82518022d8c5f6535dfa26f0be17e
Horta <nastyadsc@gmail.com>
2023-05-28 19:10:19 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp27/LessZeroException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp27;
+
+public class LessZeroException : Exception
+{
+    public LessZeroException(string name) : base($"{name} cannot be less than zero.")
+    {
+    }
+
+    public LessZeroException() : base("Value cannot be less than zero.")
+    {
+    }
+
+    public LessZeroException(string name, double value) : base($"{name} cannot be less than zero. <{name}: {value:F2}>")
+    {
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,52 @@
+﻿//4 вариант Анастасия Миронова
+using ConsoleApp27;
+
+double ConvertTime(double hours, double minutes)
+{
+    return (hours * 60 + minutes) * 60;
+}
+
+double ConvertTimeOrThrowException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new Exception("Hours cannot be less than zero");
+    if (double.IsNegative(minutes)) throw new Exception("Minutes cannot be less than zero");
+
+    return ConvertTime(hours, minutes);
+}
+
+double ConvertTimeOrThrowArgumentException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new ArgumentException("Hours cannot be less than zero");
+    if (double.IsNegative(minutes)) throw new ArgumentException("Minutes cannot be less than zero");
+
+    return ConvertTime(hours, minutes);
+}
+
+double ConvertTimeOrThrowSpecificException(double hours, double minutes)
+{
+    if (double.IsNegative(hours)) throw new LessZeroException("Hours", hours);
+    if (double.IsNegative(minutes)) throw new LessZeroException("Minutes", minutes);
+
+    return ConvertTime(hours, minutes);
+}
+
+void Main()
+{
+    Console.Write("Введите значение часов: ");
+    var hours = double.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите значение минут: ");
+    var minutes = double.Parse(Console.ReadLine()!);
+
+    try
+    {
+        var result = ConvertTimeOrThrowSpecificException(hours, minutes);
+        Console.WriteLine($"Результат преобразования: {result:F2}");
+    }
+    catch (LessZeroException exception)
+    {
+        Console.WriteLine(exception.Message);
+        throw;
+    }
+}
+Main();
